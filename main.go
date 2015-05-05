9f3ba048fb6fd9d46b4caa7ec0f0c855e2c4108f
Patrick McCarren <patrick@mmsc.io>
2015-05-05 18:09:38 -0500

    main.go: get hostname

--- main.go
+++ main.go
@@ -9,15 +9,28 @@ import (
 )
 
 func main() {
+	// parse cli flags
 	flag.Parse()
+
+	// if we are just supposed to print the version, then do so
 	if printVersion {
 		fmt.Printf("confd %s\n", Version)
 		os.Exit(0)
 	}
+
+	// load configuration
 	if err := initConfig(); err != nil {
 		log.Fatal(err.Error())
 	}
-	log.Info("Starting batond")
 
+	// get the hostname
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// check our sanity
 	sanityCheck()
+
+	log.WithField("hostname", hostname).Info("Starting batond")
 }
